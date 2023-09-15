593cb9b1f7f7e82282cb35a8556c77b36cde0bb0
Austin-Choi <austinsupra@gmail.com>
2023-09-16 08:45:44 +0900

    [Update] 서비스 소개 페이지 업데이트

--- src/components/ServiceInfo.vue
+++ src/components/ServiceInfo.vue
@@ -6,7 +6,7 @@
     <v-main>
       <v-container fluid>
         <v-col>
-          <v-row class="white--text seperate" v-bind:style="{ backgroundColor: '#263d63' }">
+          <v-row class="white--text" v-bind:style="{ backgroundColor: '#263d63' }">
             <div class="v-card__title text-h2">
               Crime-Fin?
             </div>
@@ -20,56 +20,56 @@
           <v-row>
             <v-col>
             <div class="v-card__title justify-center subtitle text-h4">
-              피싱, 생각보다 가까이 있습니다
+              <br></br>피싱, 생각보다 가까이 있습니다
             </div>
 
-            <v-row no-gutters>
+            <v-row class="separate">
               <div class="row justify-space-around"  >
-                <v-card class="mx-auto white--text" width="300" height="300" color="#263d63">
+                <v-card class="mx-auto white--text" width="300" height="300" color="#263d63" tile>
                   <v-col>
                     <div class="text-center text-h5">
-                      피해건수
+                      <br>피해건수
                     </div>
-                    <div class="icon-container ">
-                      <v-img src="../assets/thief.png" alt="로고" height="60" width="60"/>
+                    <div class="icon-container">
+                      <v-img class="mx-auto" src="../assets/thief.png" alt="로고" height="80" width="80"/>
                     </div>
                     <div class="text-center text-h6">
                       <animated-number :value="17841" :duration="1500" />건
                     </div>
-                    <div>
-                      2019년 2963건, 2020년 1만3224건, 2021년 1만 7841건으로 점차 늘어나고 있습니다.
+                    <div class="v-card__text">
+                      &nbsp;&nbsp;2019년 2963건, 2020년 1만3224건, 2021년 1만 7841건으로 점차 늘어나고 있습니다.
                     </div>
                   </v-col>
                 </v-card>
-                <v-card class="mx-auto white--text seperate" width="300" height="300" color="#263d63">
+                <v-card class="mx-auto white--text" width="300" height="300" color="#263d63" tile>
                   <v-col>
                     <div class="text-center text-h5">
-                      피해액
+                      <br>피해액
                     </div>
                     <div class="icon-container">
-                      <v-img src="../assets/won.png" alt="로고" height="60" width="60"/>
+                      <v-img class="mx-auto" src="../assets/won.png" alt="로고" height="80" width="80"/>
                     </div>
                     <div class="text-center text-h6">
                       <animated-number :value="1265" :duration="1000"/>억원
                     </div>
-                    <div>
-                      2020년 587억원, 2021년 1265억원으로 범죄 사례 뿐만 아니라 액수도 가파르게 증가했습니다.
+                    <div class="v-card__text">
+                      &nbsp;&nbsp;2020년 587억원, 2021년 1265억원으로 범죄 사례 뿐만 아니라 액수도 가파르게 증가했습니다.
                     </div>
                   </v-col>
                 </v-card>
-                <v-card class="mx-auto white--text" width="300" height="300" color="#263d63">
+                <v-card class="mx-auto white--text" width="300" height="300" color="#263d63" tile>
                   <v-col>
                     <div class="text-center text-h5">
-                      범죄 피해 증가율
+                      <br>범죄 피해 증가율
                     </div>
                     <div class="icon-container">
-                      <v-img src="../assets/ascending.png" alt="로고" height="60" width="60"/>
+                      <v-img class="mx-auto" src="../assets/ascending.png" alt="로고" height="80" width="80"/>
                     </div>
                     <div class="text-center text-h6">
                       <animated-number :value="215" :duration="500"/>%
                     </div>
-                    <div>
-                      전체적인 증가율은 전년대비 215%로 두배 이상 증가했습니다.
+                    <div class="v-card__text">
+                      &nbsp;&nbsp;전체적인 증가율은 전년대비 215%로 두배 이상 증가했습니다.
                     </div>
                   </v-col>
                 </v-card>
@@ -77,50 +77,50 @@
             </v-row>
             </v-col>
           </v-row>
-          <v-row id="btm-container" class="white--text seperate" v-bind:style="{ backgroundColor: '#263d63' }">
+          <v-row  class="white--text separate" v-bind:style="{ backgroundColor: '#263d63' }">
             <v-col>
               <div class="v-card__title justify-center subtitle text-h4">
                 주요 기능
               </div>
               <v-row no-gutters>
                 <div class="row justify-space-around" >
-                  <v-card class="mx-auto" width="300" height="300">
+                  <v-card class="mx-auto" width="300" height="300" tile>
                     <v-col>
-                      <div class="text-center text-h6">
-                        <br><br>1. 스팸 번호 판단<br><br>
+                      <div class="text-center v-card__title justify-center">
+                        1. 스팸 번호 판단
                       </div>
-                      <div>
-                        저희가 자체적으로 생성한 피싱 번호 데이터베이스를 이용하여 사용자는 번호를 입력하기만 하면 그 번호가 피싱과 연관된 스팸 번호인지 바로 알 수 있습니다.
+                      <div class="v-card__text">
+                        &nbsp;&nbsp;저희가 자체적으로 생성한 피싱 번호 데이터베이스를 이용하여 사용자는 번호를 입력하기만 하면 그 번호가 피싱과 연관된 스팸 번호인지 바로 알 수 있습니다.
                       </div>
                     </v-col>
                   </v-card>
-                  <v-card class="mx-auto" width="300" height="300">
+                  <v-card class="mx-auto" width="300" height="300" tile>
                     <v-col>
-                      <div class="text-center text-h6">
-                        <br><br>2. 피싱 링크 판단<br><br>
+                      <div class="text-center v-card__title justify-center">
+                        2. 피싱 링크 판단
                       </div>
-                      <div>
-                        VirusTotal API를 활용해서 당신의 문자나 카카오톡으로 온 수상한 링크가 스팸인지 안전한지 대신 판단해드립니다.
+                      <div class="v-card__text">
+                        &nbsp;&nbsp;VirusTotal API를 활용해서 당신의 문자나 카카오톡으로 온 수상한 링크가 스팸인지 안전한지 대신 판단해드립니다.
                       </div>
                     </v-col>
                   </v-card>
-                  <v-card class="mx-auto" width="300" height="300">
+                  <v-card class="mx-auto" width="300" height="300" tile>
                     <v-col>
-                      <div class="text-center text-h6">
-                        <br><br>3. AI 분석<br><br>
+                      <div class="text-center v-card__title justify-center">
+                        3.AI 분석
                       </div>
-                      <div>
-                        스팸 번호 판단과 피싱 링크 판단으로도 판별이 되지 않으면, 현재 고객님의 상황을 서술하여 주시면 저희가 인공지능을 활용한 자연어 처리 및 분석으로 해당 내용이 피싱과 연관되었는지 분석해드립니다.
+                      <div class="v-card__text">
+                        &nbsp;&nbsp;스팸 번호 판단과 피싱 링크 판단으로도 판별이 되지 않으면, 현재 고객님의 상황을 서술하여 주시면 저희가 인공지능을 활용한 자연어 처리 및 분석으로 해당 내용이 피싱과 연관되었는지 분석해드립니다.
                       </div>
                     </v-col>
                   </v-card>
-                  <v-card class="mx-auto" width="300" height="300">
+                  <v-card class="mx-auto" width="300" height="300" tile>
                     <v-col>
-                      <div class="text-center text-h6">
-                        <br><br>4. 각종 애프터케어<br><br>
+                      <div class="text-center v-card__title justify-center">
+                        4. 각종 애프터 케어
                       </div>
-                      <div>
-                        상황 분석 이후 고객님께 매뉴얼 제공하고 맞춤형 체크리스트를 생성합니다. 또한 개인 주소를 기반으로 은행과 경찰서를 찾아주는 스마트 지도 서비스, 피싱 예방을 위해 간단한 체험관도 제공해드리고 있습니다.
+                      <div class="v-card__text">
+                        &nbsp;&nbsp;상황 분석 이후 고객님께 매뉴얼 제공하고 맞춤형 체크리스트를 생성합니다. 또한 개인 주소를 기반으로 은행과 경찰서를 찾아주는 스마트 지도 서비스, 피싱 예방을 위해 간단한 체험관도 제공해드리고 있습니다.
                       </div>
                     </v-col>
                   </v-card>
@@ -131,7 +131,6 @@
         </v-col>
       </v-container>
     </v-main>
-
   </v-app>
 </template>
 
@@ -156,17 +155,16 @@ export default {
 
 <style >
 .icon-container{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
 }
 .subtitle{
-  margin-top: 60px;
-  margin-bottom: 60px;
+  margin-top: 10px;
+  margin-bottom: 30px;
 }
-.seperate{
-  margin-top: 50px;
-  margin-bottom: 50px;
+.separate{
+  padding-top: 50px;
+  padding-bottom: 80px;
 }
 #btm-container{
 
