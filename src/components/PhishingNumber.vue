029da256856597b6dd282ce0ae762b66c5686a61
seohee99 <imshee99@gmail.com>
2023-09-18 10:31:35 +0900

    [Feat] 보이스 피싱 조회 기능

--- src/components/PhishingNumber.vue
+++ src/components/PhishingNumber.vue
@@ -1,75 +1,79 @@
 <template>
   <v-app>
+    <AppBar />
 
-  <!-- 상단바 컴포넌트import -->
-  <AppBar />
-  
-
-  <v-main>
+    <v-main>
       <v-container>
         <v-row justify="center">
           <v-col cols="12" sm="6" md="4">
-            <!-- 전화번호 폼 -->
-            <h1>{{ $route.params.type }} :: 전화번호를 입력하세요</h1>
-            <v-form @submit.prevent="sendPhoneNumber">
-              <v-text-field
-                v-model="phoneNum"
-                label="전화번호"
-                required
-              ></v-text-field>
-              <v-btn type="submit">조회하기</v-btn>
+            <h1>{{ phishingtype }} :: 전화번호를 입력하세요</h1>
+
+            <v-form @submit.prevent="sendPhoneNumber" >
+              <v-text-field v-model="phoneNum" label="조회할 번호를 입력하세요" required></v-text-field>
+              <v-btn type="submit" dark>조회하기</v-btn>
             </v-form>
           </v-col>
         </v-row>
       </v-container>
     </v-main>
-  
+
+    <v-dialog v-model="showResultModal" width="60%" height="60%" position-x="center" position-y="center" class="result">
+      <PhishingNumberResult
+        v-if="showResultModal"
+        :responsedata="responsedata"
+        :showResultModal.sync="showResultModal"
+        :phishingtype="phishingtype"
+      />
+    </v-dialog>
+    
   </v-app>
+</template>
 
-  </template>
-  
-  <script>
-  import axios from 'axios';
-   import AppBar from '../views/AppBar.vue'; // 상단바 컴포넌트를 import 합니다.
- 
+<script>
+import axios from 'axios';
+import AppBar from '../views/AppBar.vue';
+import PhishingNumberResult from '../components/PhishingNumberResult.vue';
 
-  export default {
-    components: {
-    AppBar, // 상단바 컴포넌트를 등록합니다.
-    },
-    data() {
-      return {
-        phoneNum: '', // 사용자 입력을 저장할 변수
-      };
-    },
-    methods: {
-    sendPhoneNumber() {
-    // 사용자 입력된 전화번호를 사용하여 GET 요청을 보냅니다.
-    const phoneNum = this.phoneNum;
-    axios.get(`/phishing?phoneNumber=${phoneNum}`)
-      .then(response => {
-        // 성공적으로 요청을 보낸 경우 실행될 로직
-        console.log('GET 요청 성공:', response.data);
-
-        
-        // {{ $route.params.type }}의 값에 따라 다른 결과 페이지로 이동합니다.
-        if (this.$route.params.type === 'Message') {
-            this.$router.push({
-              name: 'MessageNumberResult',
-              query: { result: response.data },
-            });
-          } else if (this.$route.params.type === 'Phone') {
-            this.$router.push({
-              name: 'PhoneNumberResult',
-              query: { result: response.data },
-            });
-          }
-      })
-      .catch(error => {
-        // 요청 중 오류가 발생한 경우 실행될 로직
-        console.error('GET 요청 오류:', error);
-      });
+export default {
+  components: {
+    AppBar,
+    PhishingNumberResult
   },
-}}
-  </script>
-  
+
+  props: ['phishingtype'],
+
+  data() {
+    return {
+      phoneNum: '',
+      responsedata:null,
+      showResultModal:false,
+    };
+  },
+  methods: {
+    sendPhoneNumber() {
+      const phoneNum = this.phoneNum;
+
+      axios.get('http://localhost:9999/phishing', { params: { phoneNum } })
+        .then(response => {
+          console.log('GET 요청 성공:', response.data);
+          // response.data를 responseData에 저장
+          this.responsedata = response.data
+
+          // 모달 창 표시
+          this.showResultModal = true;
+
+        })
+        .catch(error => {
+          console.error('GET 요청 오류:', error);
+        })
+    },
+  },
+}
+</script>
+<style scoped>
+.result{
+  padding: 30px;
+  margin:  30px;
+}
+</style>
+
--- src/components/PhishingNumberResult.vue
+++ src/components/PhishingNumberResult.vue
@@ -1,39 +1,112 @@
+        <!--{{ this.phishingtype }} -->
+        <!-- {{ phishingtype === 'Message' ? 'Message :: 문자 번호 조회 결과' : '전화 번호 조회 결과' }} -->
+      
 <template>
-    <v-container fluid>
-      <v-layout align-center justify-center>
-        <v-flex xs12 sm8 md6>
-          <v-card>
-            <v-card-title>
-              <h2 class="display-1">전화번호 검사 결과</h2>
-            </v-card-title>
-            <v-card-text>
-              <v-alert
-                :type="result === 0 ? 'error' : 'success'"
-                outlined
-                border="top"
-                elevation="2"
-              >
-                {{ result === 0 ? '전화번호가 PhishingInfo 테이블에 없습니다.' : '전화번호가 PhishingInfo 테이블에 있습니다.' }}
-              </v-alert>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn color="primary" @click="goToMainPage">돌아가기</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </template>
-  
-  <script>
-  export default {
-    props: ['result'],
-    methods: {
-      goToMainPage() {
-        // MainPage.vue로 돌아가는 로직을 추가하세요.
-        this.$router.push('/MainPage');
-      },
+  <v-card-text v-model="showResultModal" class="total">
+    <div v-if="phishingtype === 'Phone' && responsedata">
+      <div class="resultTitle">전화 번호 조회 결과</div>
+      <p v-if="resultData === 0">
+        번호 조회 결과 해당 번호는 스팸 번호로 등록되어있지 않습니다. 안전합니다.
+      </p>
+      <p v-else>
+        번호 조회 결과 해당 번호는 스팸 번호로 등록되어 있습니다. 위험한 번호로 판단됩니다.
+      </p>
+      <p>메뉴얼을 보시겠어요?</p>
+      <v-btn @click="navigateToManual(phishingtype)" style="margin: 15px;">메뉴얼 보기</v-btn>
+      <p>AI로 더 조회해 보시겠어요?</p>
+      <v-btn @click="navigateToAICheck(phishingtype)" style="margin: 15px;">AI로 조회하기</v-btn>
+      <div style="margin: 30px;">
+      <v-btn @click="closeModal">닫기</v-btn>
+      </div>
+    </div>
+    <div v-else-if="phishingtype === 'Phone' && !responsedata">
+      <div class="resultTitle">전화 번호 조회 결과</div>
+      <p>
+        번호 조회 결과 해당 번호는 스팸 번호로 등록된 번호가 아닙니다.
+      </p>
+      <p>AI로 더 조회해 보시겠어요?</p>
+      <v-btn @click="navigateToAICheck(phishingtype)">AI로 조회하기</v-btn>
+      
+      <v-btn @click="closeModal">닫기</v-btn>
+    </div>
+    <div v-else-if="phishingtype === 'Message' && responsedata">
+      <div class="resultTitle">Message :: 문자 번호 조회 결과</div>
+      <p>
+        번호 조회 결과 해당 번호는 스팸 번호로 등록되어 있습니다. 위험한 번호로 판단됩니다.
+      </p>
+      <p>메뉴얼을 보시겠어요?</p>
+      <v-btn @click="navigateToManual(phishingtype)">메뉴얼 보기</v-btn>
+      <p>AI로 더 조회해 보시겠어요?</p>
+      <v-btn @click="navigateToAICheck(phishingtype)">AI로 조회하기</v-btn>
+      <p>링크가 있으신가요?</p>
+      <v-btn @click="navigateToLinkCheck(phishingtype)">링크 조회하기</v-btn>
+      <p>닫기</p>
+      <v-btn @click="closeModal">닫기</v-btn>
+    </div>
+    <div v-else-if="phishingtype === 'Message' && !responsedata">
+      <div class="resultTitle">Message :: 문자 번호 조회 결과</div>
+      <p>
+        번호 조회 결과 해당 번호는 스팸 번호로 등록된 번호가 아닙니다.
+      </p>
+      <p>링크가 있으신가요?</p>
+      <v-btn @click="navigateToLinkCheck(phishingtype)">링크 조회하기</v-btn>
+      <p>AI로 더 조회해 보시겠어요?</p>
+      <v-btn @click="navigateToAICheck(phishingtype)">AI로 조회하기</v-btn>
+      
+      <v-btn @click="closeModal">닫기</v-btn>
+    </div>
+    
+  </v-card-text>
+</template>
+
+
+<script>
+export default {
+  props: {
+    phishingtype: String,
+    responsedata: Object,
+    showResultModal: Boolean,
+  },
+  data() {
+    return {
+      resultData: this.responsedata.phishingId,
+    };
+  },
+  methods: {
+    closeModal() {
+      this.showResultModal = false;
+      this.$emit("update:showResultModal", false);
     },
-  };
-  </script>
-  
+    navigateToAICheck(phishingtype) {
+      // AI로 조회하기 페이지로 이동하는 로직을 추가
+      this.$router.push({ name: 'AICheck', params: {phishingtype} });
+    },
+    navigateToLinkCheck(phishingtype) {
+      // 링크 조회하기 페이지로 이동하는 로직을 추가
+      this.$router.push({ name: 'PhishingLink', params: {phishingtype} });
+    },
+    navigateToManual(phishingtype){
+      this.$router.push({ name: 'ToDoPage', params: {phishingtype} });
+    }
+  },
+};
+</script>
+
+<style scoped>
+.total{
+  background-color: white ;
+  padding: 20px; 
+  font-size:20px; 
+  text-align: center;
+}
+.resultTitle{
+  padding: 20px; 
+  margin: 20px; 
+  font-size:30px; 
+  font-weight:bolder; 
+  text-align: center;
+}
+.total{
+
+}
+</style>
