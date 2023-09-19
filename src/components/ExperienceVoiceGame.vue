3c61e529de5841dab0929708b37b09d720f477dc
Austin-Choi <austinsupra@gmail.com>
2023-09-19 10:39:01 +0900

    [Update] 피싱체험 상세페이지 appbar 초기 템플릿 적용

--- src/components/ExperienceVoiceGame.vue
+++ src/components/ExperienceVoiceGame.vue
@@ -1,12 +1,27 @@
-<script setup>
-
-</script>
-
 <template>
+  <v-app>
+    <!-- 상단바 컴포넌트 import -->
+    <AppBar />
 
+    <v-main>
+    </v-main>
+
+  </v-app>
 </template>
 
-<style scoped>
+<script>
+import AppBar from '../views/AppBar.vue'; // 상단바 컴포넌트 import
+
+export default {
+  components: {
+    AppBar, // 상단바 컴포넌트 등록
+  },
+
+};
+</script>
+
+<style >
 
 </style>
+
 <!-- /expvoicegame -->
