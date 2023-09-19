a9ed295977a3db71b069650aca3ae2626cb0c89e
0woogie <aod8948@naver.com>
2023-09-19 20:17:45 +0900

    commit -m "[FEAT] 계좌 인증 기능 추가 및 BankingVO 수정"

--- src/main/java/com/service/crimefin/controller/AssetController.java
+++ src/main/java/com/service/crimefin/controller/AssetController.java
@@ -15,38 +15,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
+import java.util.Random;
 
 @CrossOrigin(origins = { "http://localhost:3000", "http://localhost:9999" }, allowCredentials = "true")
 @Controller
 public class AssetController {
+
     @Autowired
     private AssetService assetService;
 
-    @PostMapping("/asset")
-    public ResponseEntity addAccount(@RequestBody HashMap<String, Object> requestJsonHashMap)throws Exception{
-        AccountVO accountVO = new AccountVO();
-
-        accountVO.setAccountId((String)requestJsonHashMap.get("accountId"));
-        accountVO.setMemberId((String)requestJsonHashMap.get("memberId"));
-        accountVO.setAccountNum((String)requestJsonHashMap.get("accountNum"));
-        accountVO.setBank((String)requestJsonHashMap.get("bankName"));
-        accountVO.setBalance(Integer.parseInt((String)requestJsonHashMap.get("balance")));
-        accountVO.setLimit(Integer.parseInt((String)requestJsonHashMap.get("limit")));
-
-        System.out.println(accountVO);
-
-        int result=assetService.insertAccount(accountVO);
-
-        if(result != 0) {
-            return new ResponseEntity(accountVO.getMemberId(), HttpStatus.OK);
-        }
-        else {
-            return new ResponseEntity(HttpStatus.NO_CONTENT);
-        }
-    }
-
     @PostMapping("/asset/auth")
-    public ResponseEntity authAccount(@RequestBody HashMap<String, Object> requestJsonHashMap, HttpServletRequest request) throws Exception {
+    public ResponseEntity addAccount(@RequestBody HashMap<String, Object> requestJsonHashMap, HttpServletRequest request) throws Exception {
         HttpSession session = request.getSession(false); //세션이 있으면 기존 세션 반환, 세션 없으면 null 반환
 
         //session에서 member id를 가져옴
@@ -65,18 +44,32 @@ public class AssetController {
 
         if(result != 0) { //계좌 임시로 생성 성공
 
+            Random random = new Random(); //랜덤 함수 선언
+            int createNum = 0;            //1자리 난수
+            String ranNum = "";           //1자리 난수 형변환 변수
+            int letter = 4;               //난수 자릿수: 4
+            String resultNum = "";        //결과 난수
+
+            for (int i = 0; i < letter; i++) {
+                createNum = random.nextInt(9);   //0부터 9까지 올 수 있는 1자리 난수 생성
+                ranNum = Integer.toString(createNum);  //1자리 난수를 String으로 형변환
+                resultNum += ranNum;                   //생성된 난수(문자열)을 원하는 수(letter)만큼 더하며 나열
+            }
+
             //거래내역 테이블 생성
             BankingVO bankingVO = new BankingVO();
             bankingVO.setAccountNum(accountNum);
             bankingVO.setMemberId(memberId);
             //bankingVO.setBankingDate();
-            bankingVO.setDeposit("1");
-            bankingVO.setDepositName("랜덤입금자");
+            bankingVO.setDeposit(1); //1원 송금
+            bankingVO.setDepositName(resultNum); //입금자명 입력
             //bankingVO.setBalance();
 
             int result2 = assetService.insertBanking(bankingVO);
 
             if(result2 != 0) { //거래내역 생성 성공
+                session= request.getSession(true); //세션 있으면 기존 세션 반환, 없으면 세션 새로 만듬
+                session.setAttribute("authNum", resultNum);
                 return new ResponseEntity(HttpStatus.OK);
             }
             else { //거래내역 생성 실패
@@ -91,4 +84,22 @@ public class AssetController {
 
 
     }
+
+    @PostMapping("/asset/auth/confirm")
+    public ResponseEntity checkAccount(@RequestBody HashMap<String, Object> requestJsonHashMap, HttpServletRequest request) throws Exception {
+
+        HttpSession session = request.getSession(false); //세션이 있으면 기존 세션 반환, 세션 없으면 null 반환
+
+        //session에서 authNum을 가져옴
+        String authNum = (String) session.getAttribute("authNum");
+
+        String inputNum = (String) requestJsonHashMap.get("inputNum");
+
+        if(inputNum.equals(authNum)) {
+            return new ResponseEntity(1, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(0, HttpStatus.NO_CONTENT);
+        }
+    }
+
 }
--- src/main/java/com/service/crimefin/domain/BankingVO.java
+++ src/main/java/com/service/crimefin/domain/BankingVO.java
@@ -5,24 +5,24 @@ public class BankingVO {
     private String memberId; //사용자 아이디
     private String accountNum; //계좌 번호
     private String bankingDate; //거래 일자
-    private String deposit; //예금액
+    private int deposit; //예금액
     private String depositName; //입금자 명
-    private String withDrawl; //출금액
-    private String withDrawlTo; //출금
-    private String balance; //잔고
+    private int withdrawal; //출금액
+    private String withdrawalTo; //출금
+    private int balance; //잔고
 
 
     public BankingVO() {}
 
-    public BankingVO(String bankingId, String memberId, String accountNum, String bankingDate, String deposit, String depositName, String withDrawl, String withDrawlTo, String balance) {
+    public BankingVO(String bankingId, String memberId, String accountNum, String bankingDate, int deposit, String depositName, int withdrawal, String withdrawalTo, int balance) {
         this.bankingId = bankingId;
         this.memberId = memberId;
         this.accountNum = accountNum;
         this.bankingDate = bankingDate;
         this.deposit = deposit;
         this.depositName = depositName;
-        this.withDrawl = withDrawl;
-        this.withDrawlTo = withDrawlTo;
+        this.withdrawal = withdrawal;
+        this.withdrawalTo = withdrawalTo;
         this.balance = balance;
     }
 
@@ -50,11 +50,11 @@ public class BankingVO {
         this.bankingDate = bankingDate;
     }
 
-    public String getDeposit() {
+    public int getDeposit() {
         return deposit;
     }
 
-    public void setDeposit(String deposit) {
+    public void setDeposit(int deposit) {
         this.deposit = deposit;
     }
 
@@ -66,20 +66,20 @@ public class BankingVO {
         this.depositName = depositName;
     }
 
-    public String getWithDrawl() {
-        return withDrawl;
+    public int getWithdrawal() {
+        return withdrawal;
     }
 
-    public void setWithDrawl(String withDrawl) {
-        this.withDrawl = withDrawl;
+    public void setWithdrawal(int withdrawal) {
+        this.withdrawal = withdrawal;
     }
 
-    public String getWithDrawlTo() {
-        return withDrawlTo;
+    public String getWithdrawalTo() {
+        return withdrawalTo;
     }
 
-    public void setWithDrawlTo(String withDrawlTo) {
-        this.withDrawlTo = withDrawlTo;
+    public void setWithdrawalTo(String withdrawalTo) {
+        this.withdrawalTo = withdrawalTo;
     }
 
     public String getMemberId() {
@@ -90,11 +90,11 @@ public class BankingVO {
         this.memberId = memberId;
     }
 
-    public String getBalance() {
+    public int getBalance() {
         return balance;
     }
 
-    public void setBalance(String balance) {
+    public void setBalance(int balance) {
         this.balance = balance;
     }
 
@@ -105,11 +105,11 @@ public class BankingVO {
                 ", memberId='" + memberId + '\'' +
                 ", accountNum='" + accountNum + '\'' +
                 ", bankingDate='" + bankingDate + '\'' +
-                ", deposit='" + deposit + '\'' +
+                ", deposit=" + deposit +
                 ", depositName='" + depositName + '\'' +
-                ", withDrawl='" + withDrawl + '\'' +
-                ", withDrawlTo='" + withDrawlTo + '\'' +
-                ", balance='" + balance + '\'' +
+                ", withdrawal=" + withdrawal +
+                ", withdrawalTo='" + withdrawalTo + '\'' +
+                ", balance=" + balance +
                 '}';
     }
 }
