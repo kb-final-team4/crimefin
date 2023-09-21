2be219f4ccbc525f266b04ceb290a5c9fade2682
Austin-Choi <austinsupra@gmail.com>
2023-09-21 11:33:02 +0900

    [Update] 상단바 디자인 변경

--- src/views/AppBar.vue
+++ src/views/AppBar.vue
@@ -1,17 +1,21 @@
 <template>
     <v-app-bar app color="navy" height="128" dark>
         <!-- 로고 이미지를 클릭하면 메인페이지로 이동합니다. -->
+      <v-col :cols="2">
         <router-link to="/main">
-          <v-img src="../assets/CRIME.png" alt="로고" height="120" width="240"/>
+          <v-img v-bind:style="{margin: '24px'}" src="../assets/CRIME.png" alt="로고" height="80" width="160"/>
         </router-link>
-    
+      </v-col>
+      <v-col :cols="5"></v-col>
+      <v-col :cols="3">
         <!-- 버튼 그룹 -->
-        <v-btn-toggle v-model="selectedButton" class="ml-4" >
-          <v-btn @click="navigateTo('ServiceInfo')" dark>CrimeFin?</v-btn>
-          <v-btn @click="navigateTo('Agency')" dark>가까운 기관 조회</v-btn>
-          <v-btn @click="navigateTo('ExperienceLobby')" dark>디지털 체험관</v-btn>
+        <v-btn-toggle v-model="selectedButton" v-bind:style="{float: 'right'}" >
+          <v-btn v-bind:style="{border: 'none'}" @click="navigateTo('ServiceInfo')" dark>CrimeFin?</v-btn>
+          <v-btn v-bind:style="{border: 'none'}" @click="navigateTo('Agency')" dark>가까운 기관 조회</v-btn>
+          <v-btn v-bind:style="{border: 'none'}" @click="navigateTo('ExperienceLobby')" dark>디지털 체험관</v-btn>
         </v-btn-toggle>
-    
+      </v-col>
+      <v-col :cols="2">
         <!-- 로그인한 사용자 정보 및 토글 버튼 -->
         <v-menu offset-y v-model="menuOpen">
           <template v-slot:activator="{ on, attrs }">
@@ -25,6 +29,7 @@
             <v-list-item @click="goTo('DashBoard')" >자산/대시보드</v-list-item>
           </v-list>
         </v-menu>
+      </v-col>
       </v-app-bar>
   </template>
   
