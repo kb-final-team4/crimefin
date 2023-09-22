81d6df73f42cce64c7652c1d8bb7e365757690f0
joareum <a_lme@naver.com>
2023-09-22 13:58:42 +0900

    [Feat] 마이페이지 수정

--- src/components/MemberResign.vue
+++ src/components/MemberResign.vue
@@ -1,130 +1,130 @@
-<!-- 마이페이지 속 회원탈퇴-->
 <template>
-  <div>
-    <v-navigation-drawer app>
-      <v-list>
-        <v-list-item>
-          <v-list-item-avatar>
-            <v-img src="logo.png"></v-img>
-          </v-list-item-avatar>
-          <v-list-item-title>CrimeFin</v-list-item-title>
-        </v-list-item>
-
-        <v-divider></v-divider>
-
-        <v-list-item link to="/changeinfo">
-          <v-list-item-icon>
-            <v-icon>mdi-account</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title class="larger-text">회원정보 수정</v-list-item-title>
-        </v-list-item>
-
-        <v-list-item link to="/changepassword">
-          <v-list-item-icon>
-            <v-icon>mdi-lock</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title class="larger-text">비밀번호 수정</v-list-item-title>
-        </v-list-item>
-
-        <v-list-item link to="/memberresign">
-          <v-list-item-icon>
-            <v-icon>mdi-cog</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title class="larger-text">회원탈퇴</v-list-item-title>
-        </v-list-item>
-        <!-- 추가 메뉴 아이템 작성 -->
-      </v-list>
-    </v-navigation-drawer>
-
-    <div class="black-bg4" v-if="modal4 == true"></div>
-    <div class="white-bg6" v-if="modal6 == true">
-      <h4 class="title6">탈퇴하시겠습니까?</h4>
-      <v-col cols="8">
-        <v-text-field
-          label="비밀번호 입력"
-          id="password"
-          v-model="password"
-          class="password-form"
-          required
-          outlined
-        ></v-text-field>
-      </v-col>
-      <v-btn variant="tonal" type="button" @click="confirmDelete" class="btn-delete"
-        >회원 탈퇴</v-btn
-      >
+    <div id="app">
+        <v-app>
+            <v-content>
+                <v-container fluid>
+                    <v-row>
+                        <v-col>
+                            <!-- 앱바 자리-->
+                            <AppBar fixed app/>
+                        </v-col>
+                    </v-row>
+                    <v-row no-gutters>
+                        <!-- <v-row> -->
+                        <v-col cols="2">
+                            <v-card>
+                                <v-navigation-drawer theme="dark" width="0px;" >
+                                    <v-list nav>
+                                        <v-list-item
+                                            v-for="item in items"
+                                            :key="item.title"
+                                            :to="item.to"
+                                            link
+                                        >
+                                        <v-list-item>
+                                        <v-list-item-icon>
+                                            <v-icon>{{ item.icon }}</v-icon>
+                                        </v-list-item-icon>
+                                        <v-list-item-content>
+                                            <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                            </v-list-item-content>
+                                        </v-list-item>
+                                        </v-list-item>
+                                    </v-list>
+                                </v-navigation-drawer>
+                                <v-main style="height: 850px;"></v-main>
+                            </v-card>
+                        </v-col> 
+                       
+                        <v-col cols="5" style="height: 1000px;">
+                            <div class="background">
+                              <h4 class="title6">탈퇴하시겠습니까?</h4>
+                                <v-col cols="8">
+                                  <v-text-field
+                                    label="비밀번호 입력"
+                                    id="password"
+                                    v-model="password"
+                                    class="password-form"
+                                    required
+                                    outlined
+                                  ></v-text-field>
+                                </v-col>
+                                <v-btn variant="tonal" type="button" @click="confirmDelete" class="btn-delete">회원 탈퇴</v-btn>
+                            </div>
+                        </v-col>  
+                    
+                    </v-row>
+                 </v-container>
+            </v-content>
+        </v-app>
     </div>
 
-    <app-bar />
-  </div>
+
 </template>
-
 <script>
-import axios from 'axios';
 import AppBar from '../views/AppBar.vue';
-
 export default {
-  components: {
-    'app-bar': AppBar, // 상단바 컴포넌트를 등록합니다.
-  },
-  data() {
-    return {
-      members: [],
-      password: '',
-      modal4: true,
-      modal6: true,
-    };
-  },
+    components: {
+      AppBar, // 상단바 컴포넌트를 등록합니다.
+    },  
+    name:'App',
+    data() {
+      return {
+        items: [
+        { title: "회원정보 수정", to: "/changeinfo", icon: "mdi-pencil" },
+        { title: "비밀번호 변경", to: "/delete-password", icon: "mdi-lock" },
+        { title: "회원탈퇴", to: "/memberresign", icon: "mdi-close" }
 
-  mounted() {
-    // 백엔드 API 엔드포인트 URL
-    const apiUrl = 'https://your-backend-api.com/members';
+        // 다른 아이템을 필요에 따라 추가할 수 있습니다.
+      ],
 
-    // Axios를 사용하여 API 호출
-    axios
-      .get(apiUrl)
-      .then(response => {
-        // API 응답으로부터 회원 데이터를 가져와 members에 할당
-        this.members = response.data;
-      })
-      .catch(error => {
-        console.error('회원 데이터를 가져오는 중 오류 발생:', error);
-      });
-  },
+        user: {
+        password: '',
+        newpassword: '',
+        passwordConfirm: '',
+        },
+      };
 
-  methods: {
-    confirmDelete() {
-      const passwordToCheck = this.password;
+      
+    },
 
-      const member = this.members.find(member => member.password === passwordToCheck);
+    
+    methods: {
+    async changePassword() {
+      // 현재 비밀번호, 새로운 비밀번호, 비밀번호 확인을 가져옴
+      const currentPassword = this.password;
+      const newPassword = this.newPassword;
+      const confirmPassword = this.passwordConfirm;
 
-      if (!member) {
-        alert('회원을 찾을 수 없습니다.');
-        return;
+      // 여기에서 비밀번호 변경 로직을 구현합니다.
+      // 예를 들어, 서버로 요청을 보내고 비밀번호 변경을 처리합니다.
+      try {
+        const response = await axios.post('/api/changePassword', {
+          currentPassword,
+          newPassword,
+          confirmPassword,
+        });
+
+        if (response.status === 200) {
+          alert('비밀번호가 성공적으로 변경되었습니다.');
+          this.modal4 = false; // 모달을 닫는다거나 다른 작업을 수행할 수 있음
+        } else {
+          alert('비밀번호 변경에 실패했습니다.');
+        }
+      } catch (error) {
+        console.error('비밀번호 변경 중 오류 발생:', error);
+        alert('비밀번호 변경 중 오류 발생');
       }
-
-      if (member.password !== passwordToCheck) {
-        alert('비밀번호가 일치하지 않습니다.');
-        return;
-      }
-
-      const index = this.members.indexOf(member);
-      this.members.splice(index, 1);
-      alert('회원이 성공적으로 탈퇴되었습니다.');
     },
   },
-};
-</script>
-
-<style>
-.black-bg4 {
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  position: fixed;
-  padding: 20px;
 }
+</script>
+<style>
+    .userInfo{
+        margin-top: 0px;
+    }
 
-.white-bg6 {
+    .background {
   position: fixed;
   top: 50%;
   left: 50%;
@@ -138,36 +138,9 @@ export default {
             background: white;
             border-radius: 8px;
             width: 790px;
+
             padding-top:30px;
             padding-bottom:20px;
-    }
-
-/* .white-bg6 {
-    position:absolute;
-    top:400px;
-    width: 35%; 
-    background: white;
-    border-radius: 8px;
-    padding: 20px;
-    padding-left: 200px; 
-    margin-left:700px;
-    }  */
-
-/*     
-    .btn-change{
-    margin-left:110px;
-  }
-  .title6{
-    margin-left:110px;
-    
-  }
-  .btn-delete{
-    position:absolute;
-    left:120px;
-  }
-
-  .password-form{
-    position:absolute;
-    top:15px;
-  } */
+            
+        }
 </style>
