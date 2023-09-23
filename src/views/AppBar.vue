1667f89a8d81b629c104ef5ac61bf449c356977a
seohee99 <imshee99@gmail.com>
2023-09-24 04:36:43 +0900

    [Update] 로그아웃 버튼 추가

--- src/views/AppBar.vue
+++ src/views/AppBar.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app-bar app color="navy" height="128" dark>
-        <!-- 로고 이미지를 클릭하면 메인페이지로 이동합니다. -->
+        
       <v-col :cols="2">
         <router-link to="/main">
           <v-img v-bind:style="{margin: '24px'}" src="../assets/CRIME.png" alt="로고" height="80" width="160"/>
@@ -27,6 +27,7 @@
           <v-list>
             <v-list-item @click="goTo('ChangeInfo')" >마이페이지</v-list-item>
             <v-list-item @click="goTo('DashBoard')" >자산/대시보드</v-list-item>
+            <v-list-item @click="Logout()" >로그아웃</v-list-item>
           </v-list>
         </v-menu>
       </v-col>
@@ -39,7 +40,7 @@
       return {
         selectedButton: null,
         menuOpen: false,
-        loggedInUser: this.$session.get('loginMemberName'), // 로그인한 사용자 이름을 설정합니다.
+        loggedInUser: this.$session.get('loginMemberName'), // 로그인한 사용자 이름 설정
       };
     },
     methods: {
@@ -48,22 +49,22 @@
       },
       goTo(pageName) {
         this.$router.push({ name: pageName });
-        this.menuOpen = false; // 메뉴 닫기
+        this.menuOpen = false; 
+      },
+      Logout() {
+        this.menuOpen = false; 
       },
       toggleMenu() {
-        // 메뉴 토글 버튼을 클릭했을 때 메뉴 열기/닫기 상태를 변경합니다.
         this.menuOpen = !this.menuOpen;
       },
     },
   };
   </script>
   
-  <style scoped>
-  /* 스타일을 필요에 따라 추가하세요. */
-  
-  /* 상단바 글씨 색상을 하얀색으로 변경합니다. */
+<style scoped>
+
   .v-btn, .v-list-item__title {
     color: white !important;
   }
-  </style>
+</style>
   
